88ac5d4f76c552c88d96b789b8225e374bf516ae
AliasJeff <2022248374@qq.com>
2024-07-07 21:52:55 +0800

    feat: zookeeper registry

---
+++ alias-rpc-core/src/main/java/com/alias/registry/ZooKeeperRegistry.java
@@ -0,0 +1,153 @@
+package com.alias.registry;
+
+import cn.hutool.core.collection.ConcurrentHashSet;
+import com.alias.config.RegistryConfig;
+import com.alias.model.ServiceMetaInfo;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.curator.framework.CuratorFramework;
+import org.apache.curator.framework.CuratorFrameworkFactory;
+import org.apache.curator.framework.recipes.cache.CuratorCache;
+import org.apache.curator.framework.recipes.cache.CuratorCacheListener;
+import org.apache.curator.retry.ExponentialBackoffRetry;
+import org.apache.curator.x.discovery.ServiceDiscovery;
+import org.apache.curator.x.discovery.ServiceDiscoveryBuilder;
+import org.apache.curator.x.discovery.ServiceInstance;
+import org.apache.curator.x.discovery.details.JsonInstanceSerializer;
+
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@Slf4j
+public class ZooKeeperRegistry implements Registry {
+
+    private CuratorFramework client;
+
+    private ServiceDiscovery<ServiceMetaInfo> serviceDiscovery;
+
+    private final Set<String> localRegisterNodeKeySet = new HashSet<>();
+
+    private final RegistryServiceCache registryServiceCache = new RegistryServiceCache();
+
+    private final Set<String> watchingKeySet = new ConcurrentHashSet<>();
+
+    private static final String ZK_ROOT_PATH = "/rpc/zk";
+
+
+    @Override
+    public void init(RegistryConfig registryConfig) {
+        client = CuratorFrameworkFactory
+                .builder()
+                .connectString(registryConfig.getAddress())
+                .retryPolicy(new ExponentialBackoffRetry(Math.toIntExact(registryConfig.getTimeout()), 3))
+                .build();
+
+        serviceDiscovery = ServiceDiscoveryBuilder.builder(ServiceMetaInfo.class)
+                .client(client)
+                .basePath(ZK_ROOT_PATH)
+                .serializer(new JsonInstanceSerializer<>(ServiceMetaInfo.class))
+                .build();
+
+        try {
+            client.start();
+            serviceDiscovery.start();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public void register(ServiceMetaInfo serviceMetaInfo) throws Exception {
+        serviceDiscovery.registerService(buildServiceInstance(serviceMetaInfo));
+
+        String registerKey = ZK_ROOT_PATH + "/" + serviceMetaInfo.getServiceNodeKey();
+        localRegisterNodeKeySet.add(registerKey);
+    }
+
+    @Override
+    public void unRegister(ServiceMetaInfo serviceMetaInfo) {
+        try {
+            serviceDiscovery.unregisterService(buildServiceInstance(serviceMetaInfo));
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        String registerKey = ZK_ROOT_PATH + "/" + serviceMetaInfo.getServiceNodeKey();
+        localRegisterNodeKeySet.remove(registerKey);
+    }
+
+    @Override
+    public List<ServiceMetaInfo> serviceDiscovery(String serviceKey) {
+        List<ServiceMetaInfo> cachedServiceMetaInfoList = registryServiceCache.readCache();
+        if (cachedServiceMetaInfoList != null) {
+            return cachedServiceMetaInfoList;
+        }
+
+        try {
+            Collection<ServiceInstance<ServiceMetaInfo>> serviceInstanceList = serviceDiscovery.queryForInstances(serviceKey);
+
+            List<ServiceMetaInfo> serviceMetaInfoList = serviceInstanceList.stream()
+                    .map(ServiceInstance::getPayload)
+                    .collect(Collectors.toList());
+
+            registryServiceCache.writeCache(serviceMetaInfoList);
+            return serviceMetaInfoList;
+        } catch (Exception e) {
+            throw new RuntimeException("Etcd service discovery failed", e);
+        }
+    }
+
+    @Override
+    public void heartBeat() {
+        // No need for heartbeat mechanism, establish temporary nodes, if the server fails, the temporary nodes are lost directly
+    }
+
+    @Override
+    public void watch(String serviceNodeKey) {
+        String watchKey = ZK_ROOT_PATH + "/" + serviceNodeKey;
+        boolean newWatch = watchingKeySet.add(watchKey);
+        if (newWatch) {
+            CuratorCache curatorCache = CuratorCache.build(client, watchKey);
+            curatorCache.start();
+            curatorCache.listenable().addListener(
+                    CuratorCacheListener
+                            .builder()
+                            .forDeletes(childData -> registryServiceCache.clearCache())
+                            .forChanges(((oldNode, node) -> registryServiceCache.clearCache()))
+                            .build()
+            );
+        }
+    }
+
+    @Override
+    public void destroy() {
+        log.info("Closing zookeeper client");
+        for (String key : localRegisterNodeKeySet) {
+            try {
+                client.delete().guaranteed().forPath(key);
+            } catch (Exception e) {
+                throw new RuntimeException(key + "Failed to delete", e);
+            }
+        }
+
+        if (client != null) {
+            client.close();
+        }
+    }
+
+    private ServiceInstance<ServiceMetaInfo> buildServiceInstance(ServiceMetaInfo serviceMetaInfo) {
+        String serviceAddress = serviceMetaInfo.getServiceHost() + ":" + serviceMetaInfo.getServicePort();
+        try {
+            return ServiceInstance
+                    .<ServiceMetaInfo>builder()
+                    .id(serviceAddress)
+                    .name(serviceMetaInfo.getServiceKey())
+                    .address(serviceAddress)
+                    .payload(serviceMetaInfo)
+                    .build();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
